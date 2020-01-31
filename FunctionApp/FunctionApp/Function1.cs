7808d8c9cb63a9e4dee5bdcec65e5c5d9011a27f
Marco Consolaro <marco@alcortech.net>
2020-01-31 14:51:47 +0000

    boia

--- FunctionApp/FunctionApp/Function1.cs
+++ FunctionApp/FunctionApp/Function1.cs
@@ -18,10 +18,6 @@ namespace FunctionApp
 
             string name = req.Query["name"];
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
